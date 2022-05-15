f4b623502f2a5bc82f8ad6d86c2b06377fed5a84
muratsari18 <83982092+muratsari18@users.noreply.github.com>
2022-05-15 22:56:51 +0300

    Update recorsiveMethot_ustHesaplama.java

--- recorsiveMethot_ustHesaplama.java
+++ recorsiveMethot_ustHesaplama.java
@@ -1,24 +1,28 @@
 import java.util.Scanner;
 public class recursiveMethodUsluSayi {
-    static int power (){
-      Scanner scan = new Scanner(System.in);
-        int result=1;
-        System.out.print("Taban Değerini Giriniz :");
-      int base = scan.nextInt();
-        System.out.print("Tavan Değerini Giriniz :");
-      int exp = scan.nextInt();
+    
+    static int result=1;  // dışarıda ve static yaptık yoksa görmüyordu. Main içinde
+    
+    static int power (int base, int exp){
+      
       if (exp==0)
           return 1;
+      result*=base;   //1 ile taban sayısı çarpılıyor result'a atanıyor ve result tekrar tabanla çarpılıyor
+                      // Taki exp-1 den exp miz o olana kadar o zaman return 1 oluyor. ve Recursive Metotdan çıkıyoruz.
 
+      power(base,exp-1);
 
-      for (int i =1; i<=exp; i++){
-          result*=base;
-      }
-        System.out.println(result);
-        return power();
+        return result; // Ssonuç en son return result ile main e dönüyor.
+        
+        // Normalde dönme işlemi For ile yapıyorduk ama Recursive ile buna gerek kalmadı.
 
     }
     public static void main(String[] args) {
-        System.out.println(power());
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Taban Değerini Giriniz :");
+        int base = scan.nextInt();
+        System.out.print("Tavan Değerini Giriniz :");
+        int exp = scan.nextInt();
+        System.out.println(power(base,exp));
     }
 }
