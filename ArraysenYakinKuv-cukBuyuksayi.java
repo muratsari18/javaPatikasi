99b9b3c6a0a77845b17a8058f7e33d5afc3e2558
muratsari18 <83982092+muratsari18@users.noreply.github.com>
2022-06-07 10:46:13 +0300

    Update ArraysenYakinKuv-cukBuyuksayi.java

--- ArraysenYakinKuv-cukBuyuksayi.java
+++ ArraysenYakinKuv-cukBuyuksayi.java
@@ -3,21 +3,22 @@ public class Main {
     public static void main(String[] args) {
         int [] list = {15,12,788,1,-1,-778,2,0};
 
-        int [] maxArr= new int[list.length];
+        int [] maxArr= new int[list.length]; //Büyük ve küçük sayıları tutmak için Array oluşturduk
         int [] minArr= new int[list.length];
 
         Scanner scan = new Scanner(System.in);
-        System.out.print("Bir sayi giriniz :");
+        System.out.print("Bir sayi giriniz :"); // Kullaınıcıdan sayı aldık
         int number = scan.nextInt();
-        int a = 0; int b=0;
+        int a = 0; int b=0; // Oluşan Arraylarin elemanlarını atamak için oluşturduk. For döngüsünde arttırıp her indexe bir 
+        // eleman atayacağız.
 
 
         for (int i = 0; i< list.length; i++){
-            if (list[i]>number){
+            if (list[i]>number){       //Arraydaki girilen sayıdan büyük olanları arraya attık
                 maxArr[b]=list[i];
                 b++;
             }
-            if (list[i]<number){
+            if (list[i]<number){  // küçükleri arraya attık
                 minArr[a]=list[i];
                 a++;
             }
@@ -25,18 +26,21 @@ public class Main {
 
             }
 
-        int max = maxArr[0];
-        int min=minArr[0];
+        int max = maxArr[0]; //Oluşan arrayda ilk elemanı max a
+        int min=minArr[0];   //Oluşan Arayda ilk olanı min e atadık.
 
         for (int i : maxArr)
         {
-            if (i< max && i>number){
+            if (i< max && i>number){ 
+                //MAX arrayindeki tüm elemanları teker teker geziyoruz. Eğer i indexsindeki
+                //değer ilk değerden küçük ve girdiğimiz sayıdan büyükse max a atıyoruz.
+                //Böylece en küçük büyük değeri buluyoruz.
                 max=i;
             }
 
         }
         for (int i: minArr) {
-            if (i>min && i<number){
+            if (i>min && i<number){    // Aynı işlem yapılıyor.
                 min=i;
             }
 
